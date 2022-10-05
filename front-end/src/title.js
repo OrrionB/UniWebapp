25f8b889efc5312430840673ce79ca95d1b0ad35
Orrion Barnett <Orrion.Barnett@skybettingandgaming.com>
2022-10-05 09:42:06 +0100

    first commit

---
+++ front-end/src/title.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+const Title = (props) => {
+  return <div>Heyooooo</div>;
+};
+
+export default Title; /* the default that will export from this file */
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+const { MongoClient } = require("mongodb");
+const Db = "mongodb://localhost:27017";
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+});
+
+app.listen(port, () => console.log("Listening on port 5000"));
